a9c14567aae596bb0d2d5fb812e7ce57f295a043
w1nAPI <151671891+w1nAPI@users.noreply.github.com>
2024-05-22 19:09:06 +0800

    update auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,23 +1,10 @@
 const users = require("../models/user.js");
-const bcrypt = require("bcryptjs");
 
 const login = (req, res) => {
   const { email, password } = req.body;
 
   users
-    .findOne({ email })
-    .then((user) => {
-      if (!user) {
-        return Promise.reject(new Error("Неправильные почта или пароль"));
-      }
-
-      return bcrypt.compare(password, user.password).then((matched) => {
-        if (!matched) {
-          return Promise.reject(new Error("Неправильные почта или пароль"));
-        }
-        return user;
-      });
-    })
+  .findUserByCredentials(email, password)
     .then((user) => {
       res
         .status(200)
